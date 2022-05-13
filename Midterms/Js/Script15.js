0d18e2b3a2e20783f7324158e338f89a95da7258
Josiah-Mecua <20201023@s.ubaguio.edu>
2022-05-13 21:00:52 +0800

    Exercise16-17

---
+++ Midterms/Js/Script15.js
@@ -0,0 +1,19 @@
+const First_Name = document.getElementById("FirstName");
+const Age = document.getElementsByName("Age");
+const Phone_Number = document.getElementsByName("Phone Number");
+const Email_Address = document.getElementsByName("Email Address");
+const Mailing_Address = document.getElementsByName("Mailing Address");
+const Program = document.getElementsByName("Program");
+const ID_Number = document.getElementsByName("ID Number");
+const Year_Level = document.getElementsByName("Year Level");
+const Section = document.getElementsByName("Section");
+
+console.log("First Name: " + First_Name);
+console.log("Age: " + Age);
+console.log("Phone Number" + Phone_Number);
+console.log("Email Adress: " + Email_Address);
+console.log("Mailing Address" + Mailing_Address);
+console.log("Program: " + Program);
+console.log("ID Number: " + ID_Number);
+console.log("Year Level: " + Year_Level);
+console.log("Section: " + Section);
